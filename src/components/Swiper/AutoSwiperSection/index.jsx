e93fff91b33bf6d5a516d5c111376991f8b70f3e
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-09-06 18:01:17 +0100

    something changed

--- src/components/Swiper/AutoSwiperSection/index.jsx
+++ src/components/Swiper/AutoSwiperSection/index.jsx
@@ -10,38 +10,40 @@ import "swiper/css/navigation";
 
 import "./index.css";
 import { Autoplay, Pagination, Navigation } from "swiper";
+import { useEffect } from "react";
 
 
 function AutoSwiperSection() {
-    return (
-      <>
-        <Swiper
-          // slidesPerView={2}
-          // slidesPerGroup={2}
-          spaceBetween={30}
-          centeredSlides={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
-          className="mySwiper"
-        >
-              <SwiperSlide> <Ucard /> </SwiperSlide>
-              <SwiperSlide> <Ucard /> </SwiperSlide>
-              <SwiperSlide> <Ucard /> </SwiperSlide>
-              <SwiperSlide> <Ucard /> </SwiperSlide>
-              <SwiperSlide> <Ucard /> </SwiperSlide>
-              <SwiperSlide> <Ucard /> </SwiperSlide>
-              <SwiperSlide> <Ucard /> </SwiperSlide>
-              <SwiperSlide> <Ucard /> </SwiperSlide>
-        </Swiper>
-      </>
-    );
-  }
+  const [swipers, setSwipers] = useState([]);
+  useEffect(() => {
+    fetch("api/v1").then(res => res.json()).then(data => setSwipers(data))
+  })
+  return (
+    <>
+      <Swiper
+        // slidesPerView={2}
+        // slidesPerGroup={2}
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {
+          swipers.map(swiper => {
+            <Swiper swiper={swiper} />
+          })
+        }
+      </Swiper>
+    </>
+  );
+}
 
-  export default AutoSwiperSection
+export default AutoSwiperSection
